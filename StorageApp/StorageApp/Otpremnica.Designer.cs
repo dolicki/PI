54ec0903f72392a7c4cf937c0b07778ce482c936
gdolicki <gorandolicki@gmail.com>
2017-04-24 23:37:27 +0200

    asdasda

--- StorageApp/StorageApp/Otpremnica.Designer.cs
+++ StorageApp/StorageApp/Otpremnica.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.desije = new System.Windows.Forms.Button();
             this.sadasfa = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // desije
             // 
-            this.desije.Location = new System.Drawing.Point(31, 58);
+            this.desije.Location = new System.Drawing.Point(83, 77);
+            this.desije.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.desije.Name = "desije";
-            this.desije.Size = new System.Drawing.Size(75, 23);
+            this.desije.Size = new System.Drawing.Size(100, 28);
             this.desije.TabIndex = 0;
             this.desije.Text = "button1";
             this.desije.UseVisualStyleBackColor = true;
@@ -44,18 +46,29 @@
             // sadasfa
             // 
             this.sadasfa.FormattingEnabled = true;
-            this.sadasfa.Location = new System.Drawing.Point(62, 111);
+            this.sadasfa.Location = new System.Drawing.Point(83, 137);
+            this.sadasfa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sadasfa.Name = "sadasfa";
-            this.sadasfa.Size = new System.Drawing.Size(121, 21);
+            this.sadasfa.Size = new System.Drawing.Size(160, 24);
             this.sadasfa.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(83, 185);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 2;
             // 
             // s
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.sadasfa);
             this.Controls.Add(this.desije);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "s";
             this.Text = "Otpremnica";
             this.ResumeLayout(false);
@@ -66,5 +79,6 @@
 
         private System.Windows.Forms.Button desije;
         private System.Windows.Forms.ComboBox sadasfa;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
