1f341b04093733b57e5ad80c7611e7563c05c091
gdolicki <gorandolicki@gmail.com>
2017-04-24 23:33:29 +0200

    Dodana meduskladisnica

--- StorageApp/StorageApp/AzuriranjeMeduskladisnice.Designer.cs
+++ StorageApp/StorageApp/AzuriranjeMeduskladisnice.Designer.cs
@@ -43,6 +43,9 @@
             this.unosIzlaznoSkl = new System.Windows.Forms.ComboBox();
             this.izlaznoSkl = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.datumKreiranja = new System.Windows.Forms.DateTimePicker();
+            this.sifraMeduskladisnice = new System.Windows.Forms.TextBox();
+            this.azuriraj = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -111,7 +114,7 @@
             // artikl
             // 
             this.artikl.AutoSize = true;
-            this.artikl.Location = new System.Drawing.Point(95, 222);
+            this.artikl.Location = new System.Drawing.Point(95, 223);
             this.artikl.Name = "artikl";
             this.artikl.Size = new System.Drawing.Size(39, 17);
             this.artikl.TabIndex = 23;
@@ -129,7 +132,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(85, 78);
+            this.label3.Location = new System.Drawing.Point(85, 84);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 17);
             this.label3.TabIndex = 21;
@@ -138,7 +141,7 @@
             // unosUlaznoSkl
             // 
             this.unosUlaznoSkl.FormattingEnabled = true;
-            this.unosUlaznoSkl.Location = new System.Drawing.Point(159, 148);
+            this.unosUlaznoSkl.Location = new System.Drawing.Point(159, 151);
             this.unosUlaznoSkl.Name = "unosUlaznoSkl";
             this.unosUlaznoSkl.Size = new System.Drawing.Size(274, 24);
             this.unosUlaznoSkl.TabIndex = 20;
@@ -146,7 +149,7 @@
             // ulaznoSkl
             // 
             this.ulaznoSkl.AutoSize = true;
-            this.ulaznoSkl.Location = new System.Drawing.Point(32, 151);
+            this.ulaznoSkl.Location = new System.Drawing.Point(32, 155);
             this.ulaznoSkl.Name = "ulaznoSkl";
             this.ulaznoSkl.Size = new System.Drawing.Size(111, 17);
             this.ulaznoSkl.TabIndex = 19;
@@ -155,7 +158,7 @@
             // unosIzlaznoSkl
             // 
             this.unosIzlaznoSkl.FormattingEnabled = true;
-            this.unosIzlaznoSkl.Location = new System.Drawing.Point(159, 108);
+            this.unosIzlaznoSkl.Location = new System.Drawing.Point(159, 114);
             this.unosIzlaznoSkl.Name = "unosIzlaznoSkl";
             this.unosIzlaznoSkl.Size = new System.Drawing.Size(274, 24);
             this.unosIzlaznoSkl.TabIndex = 18;
@@ -163,7 +166,7 @@
             // izlaznoSkl
             // 
             this.izlaznoSkl.AutoSize = true;
-            this.izlaznoSkl.Location = new System.Drawing.Point(32, 111);
+            this.izlaznoSkl.Location = new System.Drawing.Point(32, 118);
             this.izlaznoSkl.Name = "izlaznoSkl";
             this.izlaznoSkl.Size = new System.Drawing.Size(111, 17);
             this.izlaznoSkl.TabIndex = 17;
@@ -178,11 +181,37 @@
             this.label4.TabIndex = 31;
             this.label4.Text = "Šifra";
             // 
+            // datumKreiranja
+            // 
+            this.datumKreiranja.Location = new System.Drawing.Point(159, 78);
+            this.datumKreiranja.Name = "datumKreiranja";
+            this.datumKreiranja.Size = new System.Drawing.Size(274, 22);
+            this.datumKreiranja.TabIndex = 32;
+            // 
+            // sifraMeduskladisnice
+            // 
+            this.sifraMeduskladisnice.Location = new System.Drawing.Point(159, 43);
+            this.sifraMeduskladisnice.Name = "sifraMeduskladisnice";
+            this.sifraMeduskladisnice.Size = new System.Drawing.Size(274, 22);
+            this.sifraMeduskladisnice.TabIndex = 33;
+            // 
+            // azuriraj
+            // 
+            this.azuriraj.Location = new System.Drawing.Point(411, 546);
+            this.azuriraj.Name = "azuriraj";
+            this.azuriraj.Size = new System.Drawing.Size(132, 35);
+            this.azuriraj.TabIndex = 34;
+            this.azuriraj.Text = "Ažuriraj";
+            this.azuriraj.UseVisualStyleBackColor = true;
+            // 
             // AzuriranjeMeduskladisnice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 566);
+            this.ClientSize = new System.Drawing.Size(577, 593);
+            this.Controls.Add(this.azuriraj);
+            this.Controls.Add(this.sifraMeduskladisnice);
+            this.Controls.Add(this.datumKreiranja);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.obrisi);
             this.Controls.Add(this.label2);
@@ -223,5 +252,8 @@
         private System.Windows.Forms.ComboBox unosIzlaznoSkl;
         private System.Windows.Forms.Label izlaznoSkl;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker datumKreiranja;
+        private System.Windows.Forms.TextBox sifraMeduskladisnice;
+        private System.Windows.Forms.Button azuriraj;
     }
 }
