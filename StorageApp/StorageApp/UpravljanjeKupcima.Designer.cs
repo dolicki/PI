f3ef0c57eae2765edbfbee033a1bbc8420010c0d
gdolicki <gorandolicki@gmail.com>
2017-04-24 21:21:37 +0200

    fsdf
    fsdf

--- StorageApp/StorageApp/UpravljanjeKupcima.Designer.cs
+++ StorageApp/StorageApp/UpravljanjeKupcima.Designer.cs
@@ -32,6 +32,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -70,11 +71,21 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "Adresa";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // UpravljanjeKupcima
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(768, 453);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -92,5 +103,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
